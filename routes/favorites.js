111a6a51ea09601f17942d731832a973a637c28c
Kevin Lam <kevinlam.ucsb@gmail.com>
2017-03-10 16:25:17 -0800

    added cookie sessions

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,30 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const bcrypt = require('bcrypt-as-promised');
+const knex = require('../knex');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken')
 
-// YOUR CODE HERE
+router.get('/favorites', (req, res, next) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (err) {
+      res.send(false)
+    } else {
+      return knex('favorites')
+      .innerJoin('books', 'books.id', 'favorites.book_id')
+      .innerJoin('users', 'users.id', 'favorites.users_id')
+      .then( (favorite) => {
+        console.log(favorite)
+        res.set('Content-Type', /json/)
+        res.send(camelizeKeys(favorite))
+      }).catch( (err) => {
+        next();
+      })
+    }
+  })
+})
 
 module.exports = router;
--- routes/token.js
+++ routes/token.js
@@ -5,11 +5,58 @@ const router = express.Router();
 const bcrypt = require('bcrypt-as-promised');
 const knex = require('../knex');
 const { camelizeKeys, decamelizeKeys } = require('humps');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken')
 
 router.get('/token', (req, res, next) => {
-  knex('users').then((user) => {
-    res.send(!user ? true : false)
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (err) {
+      res.send(false)
+    } else {
+      res.send(true)
+    }
   })
+})
+
+router.post('/token', (req, res, next) => {
+  knex('users').where('email', req.body.email).then( (usersArray) => {
+    let foundUser = usersArray[0];
+    if (foundUser === undefined) {
+      res.set('Content-Type', 'text/plain')
+      res.status(400).send("Bad email or password")
+    } else {
+      bcrypt.compare(req.body.password, foundUser.hashed_password).then( (bingo) => {
+
+        const claim = { userId: 'toast' }; //this is our 'session'
+        const token = jwt.sign(claim, process.env.JWT_KEY, { //use this environment variable to sign the cookie
+          expiresIn: '7 days'  // Adds an exp field to the payload
+        });
+
+        res.cookie('token', token, {
+          httpOnly: true,
+          expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),  // 7 days
+          secure: router.get('env') === 'production'  // Set from the NODE_ENV
+        });
+
+        const responseUser = {
+          id: foundUser.id,
+          firstName: foundUser.first_name,
+          lastName: foundUser.last_name,
+          email: foundUser.email
+        };
+
+        res.status(200).send(responseUser)
+      }).catch((err) => {
+        res.set('Content-Type', 'text/plain')
+        res.status(400).send("Bad email or password")
+      })
+    }
+  })
+})
+
+router.delete('/token', (req, res, next) => {
+  res.clearCookie('token', { path: '/token'})
+  res.status(200).send(true)
 })
 
 // YOUR CODE HERE
